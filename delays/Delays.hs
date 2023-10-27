dff5102cff098a8f8752f5e06fa6ca7b99892a59
Sam Cowger <sam@galois.com>
2023-10-27 11:31:45 -0700

    Make `delay-demo` more verbose and configurable w.r.t. database/bucket name

--- delays/Delays.hs
+++ delays/Delays.hs
@@ -6,14 +6,31 @@ import Cardano.Tracer.CNSA.BlockState (averageHeaderDownloadDelays)
 import Cardano.Tracer.CNSA.BlockState.DB (initializeBlockDB, readBlockData)
 import qualified Data.Aeson as JSON
 import qualified Data.Map as Map
+import System.Environment (getArgs)
 
 main :: IO ()
 main =
-  initializeBlockDB "blocks" >>= \case
+  do
+    args <- getArgs
+    case args of
+      [dbName] -> analyze dbName
+      _ -> analyze "blocks" -- FIXME: this is hardcoded in `CnsaAnalyses`, should share
+
+analyze :: String -> IO ()
+analyze dbName =
+  initializeBlockDB dbName >>= \case
     Left err -> error err
     Right hdl ->
       do
         blockData <- readBlockData hdl
-        JSON.encodeFile "block-data.json" (Map.fromList blockData)
+        putStrLn $ "found " <> show (length blockData) <> " entries in " <> show dbName
+
         let delays = averageHeaderDownloadDelays [bd | (_hash, bd) <- blockData]
-        JSON.encodeFile "delays.json" delays
+
+        writeJSON "block-data.json" (Map.fromList blockData)
+        writeJSON "delays.json" delays
+  where
+    writeJSON file object =
+      do
+        putStrLn $ "writing to " <> file
+        JSON.encodeFile file object
